f569570324b68d5e5b1f01c46c226a29db359ca5
frankccajavilca-stack <frankccajavilca@gmail.com>
2025-09-25 13:50:16 -0500

    Eliminada carpeta frontend_reflexov3

--- frontend_reflexov3/src/utils/vars.js
+++
@@ -1,6 +0,0 @@
-import MiniLogo from '../assets/Img/Dashboard/MiniLogoReflexoDashboard.webp';
-
-const name = 'Centro de Reflexoterapia';
-const img = MiniLogo;
-
-export { name, img };
--- frontend_reflexov3/stylelint.config.js
+++
@@ -1,5 +0,0 @@
-/** @type {import('stylelint').Config} */
-export default {
-  extends: ['stylelint-config-clean-order/error'],
-  ignoreFiles: ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx'],
-};
